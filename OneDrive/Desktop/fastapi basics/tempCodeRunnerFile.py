f0f21ebe001274518b29eb31c5446cf46ec9752a
Omprakash <omprakashcn311@gmail.com>
2026-05-11 17:00:23 +0530

    Deleted Unnecessory files

--- OneDrive/Desktop/fastapi basics/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-lst = [{"Asian":"Exterior - Ace,Apex","id":1},{"Asian":"Interior - Tractor,Premimium,Royale","id":2}]
-def find_details(id):
-    for items in lst:
-        if items["id"]==id:
-            print(items)
-find_details(lst)
